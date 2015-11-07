9be6accb8e7d6798f1512bf8b0240f979f6cff3c
Grant <gtumen@gmail.com>
2015-11-07 07:06:14 -0600

    Added 2 Functions, Modified 2 Functions
    Added GetOverrideIndex()
    Added RecordByLoadOrderFormID()
    
    Modified WinningOverrideBefore() to use GetOverrideIndex() and added a allowMasters boolean as a parameter to determine what record to return.
    
    Proposed a different approach to OverrideByFile, which, if used, would utilize RecordByLoadOrderFormID.
    I didn't integrate RecordByLoadOrderFormID into OverrideByFile yet just in case the new approach is thrown out.

--- Edit Scripts/lib/mteRecords.pas
+++ Edit Scripts/lib/mteRecords.pas
@@ -81,18 +81,15 @@ begin
   Result := GetFlag(element, index);
 end;
 
+
 //When given a Record and a File, will try and find the override found in the given file
 function OverrideByFile(element, file: IInterface): IInterface;
 var
   ovCount, i: Integer;
   ovByIndex: IInterface;
 begin
+  Result := nil;
   ovCount := OverrideCount(element);
-  //check if there is even any overrides to begin with.  If not, notify the user and exit the function.
-  if ovCount == 0 then begin
-    //AddMessage('OverrideByFile Error: No Overrides for element ' + ShortName(element)');
-    Exit;
-  end;
   //Parse through all overrides and check if the file of the override matches the given file parameter
   for i := 0 to Pred(ovCount) do begin
     ovByIndex := OverrideByIndex(element, i);
@@ -104,32 +101,85 @@ begin
   end;
 end;
 
-//When given an Override of a record, will try to get the previous override, otherwise will return the given record
-function WinningOverrideBefore(element: IInterface): IInterface;
+{
+Alternative OverrideByFile Function
+  Unlike the other functions I have gone ahead and tested this one and it does work.
+  Pros:
+  -Doesn't require parsing
+  -Most of the calculations are done in native TES5Edit code
+  
+  Cons:
+  -Requires the function to be cast in a try statement as LoaddOrderFormIDToFileFormID() wil throw an error
+   if the file you give as a parameter does not have the appropriate file as a master. 
+   Not sure how much that may affect speeds.
+   
+  Not quite sure which is faster so I will leave it up to you.
+}
+function OverrideByFile2(element, file: IInterface): IInterface;
 var
-  ovCount, i: Integer;
+  fFormID: Integer;
+begin
+  Result := nil;
+  try
+    //Checks if the file is capable of having "element" as an override record and, if so, it wil give the appropriate FileFormID.
+    fFormID := LoadOrderFormIDToFileFormID(file, GetLoadOrderFormID(element));
+    Result := RecordByFormID(file, fFormID, false);
+  except
+    on e:exception do begin
+      //AddMessage('OverrideByFile2: Override ' + IntToHex(loFormID,8) + ' not found in file ' + GetFileName(file));
+    end;
+  end;
+end;
+{
+ An extension of TES5Edit's native function RecordByFormID().  
+ Allows you to input LoadOrderFormIDs rather than FileFormIDs
+ Derived this function using the code above.  Should be a useful function.
+ Note: Havent integrated this into the function above yet just in case we decide not to use it.
+}
+function RecordByLoadOrderFormID(file: IInterface; loFormID: cardinal; allowOverrides: Boolean): IInterface;
+var
+  fFormID: Integer;
+begin
+  Result := nil;
+  try
+    fFormID := LoadOrderFormIDToFileFormID(file, loFormID);
+    Result := RecordByFormID(file,fFormID,allowOverrides);
+  except
+    on e:Exception do begin
+      //AddMessage('RecordByLoadOrderFormID Warning: Record ' + IntToHex(loFormID,8) + ' not found in ' + GetFileName(file));
+    end;
+  end;
+  //RecordByFormID() will return nil if the record cannot be found.
+end;
+
+//Will return the override index of a given Record, will return -1 if given a Master Recrod
+function GetOverrideIndex(element: IInterface): Integer;
+var
+  i, ovCount: Integer;
 begin
   ovCount := OverrideCount(element);
-  //If the given record is the only override or the master then return the given record,
-  if ovCount < 2 then begin
-    Result := element;
-    //AddMessage('WinningOverride Error: ' + IntToStr(ovCount) + ' overrides found for ' + ShortName(element) + '.  Returning given Element');
-    Exit;
-  end;
-  //Parse through the list until your record is found
-  for i := Pred(ovCount) downto 0 do begin
-    //If your element is found then return the previous override
-    if Equals(OverrideByIndex(element, i), element) then begin
-      Result := OverrideByIndex(element, (i-1));
-      Exit;
+  Result := -1;
+  for i := 0 to Pred(ovCount) do begin
+    If Equals(OverrideByIndex(element, i), element) then begin
+      Result := i;
     end;
   end;
 end;
 
-
-
-
-
-
-
+//When given an Override of a record, will try to get the next winning override.  
+//If the provided record is the only override and allowMasters is true, then it will return the Master record, otherwise it will return itself.
+function WinningOverrideBefore(element: IInterface; allowMasters: Boolean): IInterface;
+var
+  ovIndex, i: Integer;
+begin
+  ovIndex := GetOverrideIndex(element)
+  Case ovIndex of
+    -1 : raise Exception.Create('WinningOverrideBefore: Provided Record is a Master Record');
+     0 : begin
+          if AllowMasters then Result := Master(element) 
+          else Result := element;
+         end;
+    else Result := OverrideByIndex(element, Pred(ovIndex));
+  end;
+end;
 end.
